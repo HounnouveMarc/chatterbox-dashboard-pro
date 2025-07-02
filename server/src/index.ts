a2be29ea6f034b8dc53a566d95f49c4cccf04495
HounnouveMarc <houmarc17@gmail.com>
2025-07-02 12:32:37 +0100

    Mise à jour du fichier index.ts pour la seconde fois

--- server/src/index.ts
+++ server/src/index.ts
@@ -57,6 +57,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'whatsapp_bot',
+  ssl: { rejectUnauthorized: false }
 });
 
 // Tester la connexion à la base de données
